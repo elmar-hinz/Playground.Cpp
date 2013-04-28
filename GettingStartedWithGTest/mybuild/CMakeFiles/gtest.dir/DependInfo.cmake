96f8eb6f8090f037bc00837bd13ff07a5ff08882
Elmar Hinz <t3elmar@googlemail.com>
2013-04-28 12:30:48 +0200

    building gtest

---
+++ GettingStartedWithGTest/mybuild/CMakeFiles/gtest.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/gtest/src/gtest-all.cc" "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/mybuild/CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/gtest/include"
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/gtest"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ GettingStartedWithGTest/mybuild/CMakeFiles/sample3_unittest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/gtest/samples/sample3_unittest.cc" "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/mybuild/CMakeFiles/sample3_unittest.dir/samples/sample3_unittest.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/mybuild/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/mybuild/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/gtest/include"
+  "/Users/elmar/Playground.Cpp/GettingStartedWithGTest/gtest"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
